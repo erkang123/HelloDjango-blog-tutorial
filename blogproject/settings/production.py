d81c3d4300d8f063e7c6028255e2ebc848590c25
erkang123 <1226973520@qq.com>
2020-04-07 18:16:35 +0800

    1111

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -7,5 +7,5 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
 
 # ALLOWED_HOSTS = ['.kanryblog.com']
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 HAYSTACK_CONNECTIONS['default']['URL'] = 'http://elasticsearch:9200/'
