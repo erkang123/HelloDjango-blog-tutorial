6542f4f2c1ac00ed87f37aa59a9f35b86de4607c
erkang123 <1226973520@qq.com>
2020-04-07 12:23:00 +0800

    99889

--- compose/production/django/start.sh
+++ compose/production/django/start.sh
@@ -2,4 +2,5 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn blogproject.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/app
+#gunicorn blogproject.wsgi:application -w 4 -k gthread -b 0.0.0.0:8000 --chdir=/app
+gunicorn blogproject.wsgi:application -c gunicorn.conf
